265e62c75252f97e030c3274345fa4d4d66b5967
Gregory Murray <gr.j.murray@gmail.com>
2024-11-02 10:43:27 -0700

    wrote functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "."},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+
+function superbowlWin(array){
+    if (array.find(obj=>obj.result==="W")===undefined) return array.find(obj=>obj.result==="W")
+   const yearValue = array.find(obj=>obj.result==="W").year
+   console.log(typeof yearValue)
+   return yearValue
+}
+
+superbowlWin(record)
